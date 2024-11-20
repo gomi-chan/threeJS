6bfc8070d8927cf05032ee9cbcd315381f4d3e91
gomi-chan <nakagomi@flourish-group.com>
2024-11-20 21:51:01 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { ViteHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   plugins: [
-    createHtmlPlugin({
-      inject: {
-        injectData: {
-          title: "月の満ち欠け",
-          description: "WebGLで表現した月の満ち欠け",
-          ogImage: "/images/ogp.jpg",
+    ViteHtmlPlugin(),
+    // Assuming you want to use createHtmlPlugin to inject metadata
+    {
+      ...createHtmlPlugin({
+        inject: {
+          injectData: {
+            title: "月の満ち欠け",
+            description: "WebGLで表現した月の満ち欠け",
+            ogImage: "/images/ogp.jpg",
+          },
         },
-      },
-    }),
+      }),
+    },
   ],
 });
