54c68f7a0fe8886490e8d9168d0f3e179a7883ea
gomi-chan <nakagomi@flourish-group.com>
2024-11-20 20:41:10 +0900

    Add files with LFS tracking

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "node_modules/three/examples/jsm/controls/OrbitControls.js";
 import GUI from "https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm";
 
 // UI
